7bc7169f8374a992cc6599bb88cd9c32524b80f2
Hosmairys <holderhosmairys@gmail.com>
2023-07-13 21:59:37 -0300

    Exercicios da tarefa2

--- Tarefa.js
+++ Tarefa.js
@@ -1,4 +1,7 @@
-// 2
+// 2- Uma empresa decide dar aumento de 30% aos funcionários cujo salário é inferior a 500 reais. Escreva um programa que receba o
+//salário de um funcionário e imprima o valor do salário reajustado ou uma mensagem caso o funcionário não tenha direito a aumento.
+
+
  let prompt = require("prompt-sync")()
 
 // let numero = prompt("Digite seu salario: ")
@@ -10,7 +13,7 @@
 // }
 
 
-//5Faça um programa receba dois valores e imprima qual é o maior número digitado.
+//5- Faça um programa receba dois valores e imprima qual é o maior número digitado.
 
 
 
@@ -23,31 +26,64 @@
 //     console.log("O segundo numero é maior que o primeiro")
 // }
 
-//6 Faça um programa que receba 4 notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de APROVADO
+//6- Faça um programa que receba 4 notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de APROVADO
 //para média superior ou igual a 7,0 RECUPERAÇÃO para notas entre
 //5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
  
 
-let nota1 = parseInt(prompt("Digite seus pontos do examen: "))
-let nota2 = parseInt(prompt("Digite seus pontos do examen: "))
-let nota3 = parseInt(prompt("Digite seus pontos do examen: "))
-let nota4 = parseInt(prompt("Digite seus pontos do examen: "))
+// let nota1 = parseInt(prompt("Digite seus pontos do examen: "))
+// let nota2 = parseInt(prompt("Digite seus pontos do examen: "))
+// let nota3 = parseInt(prompt("Digite seus pontos do examen: "))
+// let nota4 = parseInt(prompt("Digite seus pontos do examen: "))
 
-media = (nota1 + nota2 + nota3 + nota4)/4 ; 
+// media = (nota1 + nota2 + nota3 + nota4)/4 ; 
 
-if ( media >= 7,0 ) 
-{
-    console.log ("Aprovado");
+// if ( media >= 7,0 ) 
+// {
+//     console.log ("Aprovado");
 
-} 
-else 
-{ 
-    if ( media < 5,0 ) 
-    {
-       console.log("Reprovado");
-    }
-    else 
-    {
-       console.log ("Recuperação");
-    }
-}
+// } 
+// else 
+// { 
+//     if ( media < 5,0 ) 
+//     {
+//        console.log("Reprovado");
+//     }
+//     else 
+//     {
+//        console.log ("Recuperação");
+//     }
+// }
+
+
+//1- Faça um programa que receba o nome, cargo e salário de um
+// funcionário. Se o funcionário ganhar abaixo de 1000 reais, calcule o
+// salário acrescido de 10%. Ao final exiba o nome, o cargo e o salário
+// desse funcionário.
+
+// let nome = (prompt("Digite o seu nome: "))
+// let cargo = (prompt("Digite seu cargo: "))
+// let salario = (prompt("Digite o seu salario: "))
+// let acrescido = parseInt(prompt("Digite o valor do acrescido: "))
+
+// if (salario <= 100) {
+//     let novoSalario = salario+(salario(acrescido/100))
+//     console.log(nome+", o seu salario hojé é R$"+salario+"na função de"+cargo)
+// }
+
+
+
+
+//3- Faça um programa que receba um número inteiro do usuário e informe se este número é positivo ou negativo.
+
+// let numero = (prompt("Digite o numero: "))
+
+// if (numero > 0) {
+//     console.log("O numero digito é positivo ")
+// }else if( numero < 0){
+//     console.log("O numero digito é negativo ")
+// }else{
+//     console.log("O numero digitado é igual a cero")
+// }
+
+
